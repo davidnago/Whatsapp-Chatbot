2a89c01909d2783bc5982ce6ac6c932f40874dbb
David Nago <davidr99.nago@gmail.com>
2024-05-19 16:34:18 +0200

    ChatBot chat flow  configuration

--- src/main/java/zw/co/dcl/engine/whatsapp/WhatsappApplication.java
+++ src/main/java/zw/co/dcl/engine/whatsapp/WhatsappApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"zw.co.dcl.engine.whatsapp"})
-public class WhatsappApplication {
+public class    WhatsappApplication {
     public static void main(String[] args) {
         SpringApplication.run(WhatsappApplication.class, args);
     }
